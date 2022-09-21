09eb630b605367c4a768bc4fdf1daffee613bfbe
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-09-21 20:05:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+
+<body>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, quae facilis eum ex deleniti aut commodi
+        aliquid tempore consequuntur voluptate perspiciatis, pariatur voluptates non ut alias velit error tempora esse.
+    </h1>
+</body>
+
+</html>
