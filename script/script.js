f65ed866be1d5f6e18de475f7a2f87fc98eb3218
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-09-22 10:28:20 +0530

    fixed

--- script/script.js
+++ script/script.js
@@ -1,10 +1,24 @@
 var loc = document.querySelector(".form-control")
-var btn=document.querySelector
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(res => {
-        loc.value = JSON.stringify(res.coords.latitude) + " " + JSON.stringify(res.coords.longitude)
-    })
+var btn = document.querySelector(".fs-5.btn.btn-secondary");
+
+// 17b2aa06e2fc32be5d7db17960eb8f30
+
+btn.addEventListener('click', () => {
+    btn.innerHTML = "Getting Location...";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(response, error);
+    }
+    else {
+        alert("GeoLocation is not supported by your bowers. Please Try opening in different one.")
+    }
+})
+function response(res) {
+    setTimeout(() => {
+        btn.innerHTML = "Location Updated"
+        const { latitude, longitude } = res.coords;
+        loc.value = "latitude : " + latitude + " longitude: " + longitude;
+    }, 5000);
 }
-else {
-    alert("Not")
+function error(err) {
+    loc.value = err.message
 }
