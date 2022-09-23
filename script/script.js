58b014fcb5e383f4a1ee9fb766c46590e76877f1
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-09-23 18:42:09 +0530

    added

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ var loc = document.querySelector(".form-control");
 var city = document.querySelector("#city");
 var btn = document.querySelector(".fs-5.btn.btn-secondary");
 var update = document.querySelector('.loading');
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}
 
 const weatherUrl = "https://api.openweathermap.org/data/2.5/";
 const apiKey = "&appid=17b2aa06e2fc32be5d7db17960eb8f30";
@@ -25,7 +25,7 @@ btn.addEventListener('click', () => {
         navigator.geolocation.getCurrentPosition(response, error);
     }
     else {
-        alert("GeoLocation is not supported by your Bowers. Please try opening in different one.")
+        getWeatherByName("Delhi");
     }
 })
 
@@ -54,30 +54,34 @@ function response(res) {
 
 city.addEventListener('keydown', res => {
     if (res.key == "Enter" && city.value != "") {
-        update.style.display = "flex";
-        update.innerHTML = "Getting Location.....";
-        const todayWeather = weatherUrl + `weather?q=${city.value}&units=metric` + apiKey;
-        const forecastWeather = weatherUrl + `forecast/daily?q=${city.value}&cnt=7&units=metric` + apiKey;
-        fetch(todayWeather).then(res => {
-            res.json().then(rr => {
-                setTimeout(() => {
-                    temp.innerText = rr.main.temp;
-                    weType.innerText = rr.weather[0].description;
-                    weLocation.innerText = rr.name;
-                    feeltemp.innerText = rr.main.feels_like;
-                    humidity.innerText = rr.main.humidity + "%";
-                    getlocation.style.display = "none";
-                    weatherbox.style.display = "flex"
-                }, 5000);
-            })
-        })
+        getWeatherByName(city.value);
     }
 })
 
+function getWeatherByName(name) {
+    update.style.display = "flex";
+    update.innerHTML = "Getting Location.....";
+    const todayWeather = weatherUrl + `weather?q=${name}&units=metric` + apiKey;
+    const forecastWeather = weatherUrl + `forecast/daily?q=${name}&cnt=7&units=metric` + apiKey;
+    fetch(todayWeather).then(res => {
+        res.json().then(rr => {
+            setTimeout(() => {
+                temp.innerText = rr.main.temp;
+                weType.innerText = rr.weather[0].description;
+                weLocation.innerText = rr.name;
+                feeltemp.innerText = rr.main.feels_like;
+                humidity.innerText = rr.main.humidity + "%";
+                getlocation.style.display = "none";
+                weatherbox.style.display = "flex"
+            }, 5000);
+        })
+    })
+}
 //-------------------------------------------------------------------------------------------------------------//
 
 function error(err) {
-    loc.value = err.message
+    loc.value = "Location Permission Denied, Taking Delhi as Default.";
+    getWeatherByName("delhi");
 }
 document.querySelector('.temp').addEventListener('click', r => {
 
