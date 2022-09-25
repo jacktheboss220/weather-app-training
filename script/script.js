9d4f32f11c4d2d68de7c3b9d1707b79f8fc80a1f
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-09-25 18:39:47 +0530

    change the api

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ var weather = document.querySelector('.weather');
 var loc = document.querySelector(".form-control");
 var city = document.querySelector("#city");
 var btn = document.querySelector(".fs-5.btn.btn-secondary");
+var date = document.querySelector('#date');
 var update = document.querySelector('.loading');
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}
-
+//-------------------------------------------------------------------------------------------------------------//
 const weatherUrl = "https://api.openweathermap.org/data/2.5/";
 const apiKey = "&appid=17b2aa06e2fc32be5d7db17960eb8f30";
 
@@ -37,19 +37,9 @@ btn.addEventListener('click', () => {
 function response(res) {
     const { latitude, longitude } = res.coords;
     const todayWeather = weatherUrl + `weather?lat=${latitude}&lon=${longitude}&units=metric` + apiKey;
-    const forecastWeather = weatherUrl + `forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&units=metric` + apiKey;
     fetch(todayWeather).then(r => {
         r.json().then(rr => {
-            setTimeout(() => {
-                temp.innerText = rr.main.temp;
-                weType.innerText = rr.weather[0].description;
-                weLocation.innerText = rr.name;
-                feeltemp.innerText = rr.main.feels_like;
-                humidity.innerText = rr.main.humidity + "%";
-                windSpeed.innerText = rr.wind.speed;
-                getlocation.style.display = "none";
-                weather.style.display = "flex"
-            }, 3000);
+            newApi(rr.name);
         })
     })
 }
@@ -66,25 +56,27 @@ function getWeatherByName(name) {
     name = name.trim();
     update.style.display = "flex";
     update.innerHTML = "Getting Location.....";
-    const todayWeather = weatherUrl + `weather?q=${name}&units=metric` + apiKey;
-    const forecastWeather = weatherUrl + `forecast/daily?q=${name}&cnt=7&units=metric` + apiKey;
-    fetch(todayWeather).then(res => {
-        res.json().then(rr => {
-            setTimeout(() => {
-                temp.innerText = rr.main.temp;
-                weType.innerText = rr.weather[0].description;
-                weLocation.innerText = rr.name;
-                feeltemp.innerText = rr.main.feels_like;
-                humidity.innerText = rr.main.humidity + "%";
-                windSpeed.innerText = rr.wind.speed;
-                getlocation.style.display = "none";
-                weather.style.display = "flex"
-            }, 3000);
-        })
-    })
+    newApi(name);
 }
 //-------------------------------------------------------------------------------------------------------------//
 
+function newApi(city) {
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next7days?unitGroup=metric&include=days%2Ccurrent&key=HVBQXSJ9ZPRMSUZJXEHSXS2CP&contentType=json`).then(r => {
+        r.json().then(res => {
+            setTimeout(() => {
+                date.innerText = res.days[0].datetime;
+                temp.innerText = res.days[0].temp;
+                weType.innerText = res.days[0].conditions;
+                weLocation.innerText = res.resolvedAddress;
+                feeltemp.innerText = res.days[0].feelslike;
+                humidity.innerText = res.days[0].humidity + "%";
+                windSpeed.innerText = res.days[0].windspeed;
+                getlocation.style.display = "none";
+                weather.style.display = "flex"
+            }, 3000)
+        })
+    })
+}
 function error(err) {
     loc.value = "Location Permission Denied, Taking Delhi as Default.";
     getWeatherByName("delhi");
